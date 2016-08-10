826d0dcc844f441488b4dc95c575f7003837ab21
Guoqing <zhaogq10@163.com>
2016-08-10 07:43:43 +0530

    Product api, didn't return archived products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,18 +1,17 @@
 class Api::V1::ProductsController < ApiController
 
   def index
-    products = Tag.find_by_display(params['tag']) ? Tag.find_by_display(params['tag']).products.where(on_sale: true) : []
+    products = Tag.find_by_display(params['tag']) ? Tag.find_by_display(params['tag']).products.where(on_sale: true, archive: false) : []
     products = products.to_a.sort_by{|product| product.display_order}.reverse
     @products = paginate products, per_page: params[:limit] || 10
   end
 
   def show
     @product = Product.find(params['id'].to_i)
-    raise ActiveRecord::RecordNotFound, '此商品已下架' unless @product.on_sale
   end
 
   def search
-    products = Product.where("name like ?", "%#{params[:key_word]}%").where(on_sale: true)
+    products = Product.where("name like ?", "%#{params[:key_word]}%").where(on_sale: true, archive: false)
     @products = paginate products, per_page: params[:limit] || 10
     render :index
   end
