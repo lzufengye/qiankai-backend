3f9f143dedb44ead8e2fc269605b9a5b717ae5c5
Guoqing <zhaogq10@163.com>
2016-11-12 23:28:56 +0800

    Hot fix for database pool and worker_processes for unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,1 +1,15 @@
+worker_processes 3
+
 listen ENV['PORT'] || 8080, :tcp_nopush => true
+
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+end
